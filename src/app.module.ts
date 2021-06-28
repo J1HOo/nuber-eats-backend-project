311b22416522cecdddf546e58b0cf4abe3b6c0ad
박지호 <businessj1ho@gmail.com>
2021-06-28 11:10:20 +0900

    DB set up fin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module, Query } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
@@ -7,29 +9,33 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+      DB_HOST: Joi.string().required(),
+      DB_PORT: Joi.string().required(),
+      DB_USERNAME: Joi.string().required(),
+      DB_PASSWORD: Joi.string().required(),
+      DB_NAME: Joi.string().required(),
+      })
+    }),
     TypeOrmModule.forRoot({
-      "type": "postgres",
-      "host": "localhost",
-      "port": 5432,
-      "username": "parkjiho",
-      "password": "0000",
-      "database": "nuber-eats",
-      "synchronize": true,
-      "logging": true,
-      "entities": [
-         "src/entity/**/*.ts"
-      ],
-      "migrations": [
-         "src/migration/**/*.ts"
-      ],
-      "subscribers": [
-         "src/subscriber/**/*.ts"
-      ]
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      name: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
     }),
     GraphQLModule.forRoot({
     autoSchemaFile: true,
   }), 
+  RestaurantsModule,
 ],
   controllers: [],
   providers: [],
