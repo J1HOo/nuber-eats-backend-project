2fbba92c6373d5b41e862e3b409c8039d83fdd99
박지호 <businessj1ho@gmail.com>
2021-07-03 18:50:40 +0900

    main- 4.6 An Alternative Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,15 +8,16 @@ import { User } from "./entities/user.entity";
 export class UserService{
     constructor(@InjectRepository(User) private readonly users: Repository<User>) {}
 
-    async createAccount({email, password, role}: CreateAccountInput): Promise<string | undefined> {
+    async createAccount({email, password, role}: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
         try{
             const exists = await this.users.findOne({ email });
             if (exists) {
-                return '해당 이메일을 가진 사용자가 이미 존재합니다.';
+                return { ok: false, error: '해당 이메일을 가진 사용자가 이미 존재합니다.' };
             }
             await this.users.save(this.users.create({ email, password, role }));
+            return { ok: true };
         } catch(e){
-            return '계정을 생성할 수 없었습니다.';
+            return { ok: false, error: '계정을 생성할 수 없었습니다.' };
         }
     }
 }
