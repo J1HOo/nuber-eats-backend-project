c05c8394da4fc78e412f58d7d139adec38067dea
박지호 <businessj1ho@gmail.com>
2021-07-11 22:32:55 +0900

    main- 10.4 Roles part One

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -7,63 +7,54 @@ import { IsBoolean, IsEmail, IsEnum, IsString } from "class-validator";
 import { Restaurant } from "src/restaurants/entities/restaurant.entity";
 
 
-enum UserRole{ Client, Owner, Delivery } 
+export enum UserRole { Client = 'CLIENT', Owner = 'OWNER', Delivery = 'DELIVERY', }
 
-registerEnumType(UserRole, { name: 'UserRole' })
-
+registerEnumType(UserRole, { name: 'UserRole' });
 @InputType('UserInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
-export class User extends CoreEntity{
-
-    @Column({ unique: true })
-    @Field(type => String)
-    @IsEmail()
-    email: string;
-
-    @Column({ select: false })
-    @Field(type => String)
-    @IsString()
-    password: string;
-
-    @Column({ type: 'enum', enum: UserRole })
-    @Field(type => UserRole)
-    @IsEnum(UserRole)
-    role: UserRole;
-
-    @Column({ default: false })
-    @Field(type => Boolean)
-    @IsBoolean()
-    verified: boolean;
-
-    @Field(type => [Restaurant])
-    @OneToMany(
-      type => Restaurant,
-      restaurant => restaurant.owner,
-    )
-    restaurants: Restaurant[];
-  
-
-    @BeforeInsert()
-    @BeforeUpdate()
-    async hashPassword(): Promise<void> {
-        if (this.password) {
-            try {
-              this.password = await bcrypt.hash(this.password, 10);
-            } catch (e) {
-              console.log(e);
-              throw new InternalServerErrorException();
-            }
-        }
+export class User extends CoreEntity {
+  @Column({ unique: true })
+  @Field(type => String)
+  @IsEmail()
+  email: string;
+  @Column({ select: false })
+  @Field(type => String)
+  @IsString()
+  password: string;
+  @Column({ type: 'enum', enum: UserRole })
+  @Field(type => UserRole)
+  @IsEnum(UserRole)
+  role: UserRole;
+  @Column({ default: false })
+  @Field(type => Boolean)
+  @IsBoolean()
+  verified: boolean;
+  @Field(type => [Restaurant])
+  @OneToMany(
+    type => Restaurant,
+    restaurant => restaurant.owner,
+  )
+  restaurants: Restaurant[];
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    if (this.password) {
+      try {
+        this.password = await bcrypt.hash(this.password, 10);
+      } catch (e) {
+        console.log(e);
+        throw new InternalServerErrorException();
+      }
     }
-
-    async cheakPassword(aPassword: string): Promise<boolean> {
-        try {
-           const ok = await bcrypt.compare(aPassword, this.password);
-           return ok;
-        } catch(e) {
-            console.error(e);
-            throw new InternalServerErrorException()
-        }
+  }
+  async checkPassword(aPassword: string): Promise<boolean> {
+    try {
+      const ok = await bcrypt.compare(aPassword, this.password);
+      return ok;
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException();
     }
+  }
 }
