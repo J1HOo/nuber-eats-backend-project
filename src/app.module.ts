e1e198425435bb10389e2835652ff34e52c078ec
박지호 <businessj1ho@gmail.com>
2021-07-11 22:06:11 +0900

    main- 10.1 Relationships and InputTypes

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { Verification } from './users/entities/verification.entity';
 import { MailModule } from './mail/mail.module';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { Category } from './restaurants/entities/cetegory.entity';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 
 @Module({
@@ -60,6 +61,7 @@ import { Category } from './restaurants/entities/cetegory.entity';
     fromEmail: process.env.MAILGUN_FROM_EMAIL,
   }),
   UsersModule,
+  RestaurantsModule,
 ],
   controllers: [],
   providers: [],
