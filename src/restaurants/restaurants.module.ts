e08bac03dbd4c0d2ad0ed616d2282f7835d771d0
박지호 <businessj1ho@gmail.com>
2021-07-14 22:54:24 +0900

    main- 11.2 Create Dish part Two

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dish } from './entities/dish.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryRepository } from './repositories/category.repository';
 import { CategoryResolver, DishResolver, RestaurantResolver } from './restaurants.resolver';
@@ -7,7 +8,7 @@ import { RestaurantService } from './restaurants.service';
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
+    imports: [TypeOrmModule.forFeature([Restaurant, Dish, CategoryRepository])],
     providers: [
         RestaurantResolver, CategoryResolver, DishResolver, RestaurantService,],
 })
