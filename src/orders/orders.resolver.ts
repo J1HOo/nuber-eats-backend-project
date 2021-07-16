f98d35e5127b3dbfed865a19dfef566fc0ef9db3
박지호 <businessj1ho@gmail.com>
2021-07-16 20:16:25 +0900

    main- 12.6 Subscription Resolve

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -66,6 +66,8 @@ export class OrderResolver {
     filter: ({ readyPotato }, { potatoId }) => {
       return readyPotato === potatoId;
     },
+    resolve: ({ readyPotato }) =>
+    `Your potato with the id ${readyPotato} is ready!`,
   })
   @Role(['Any'])
   readyPotato(@Args('potatoId') potatoId: number) {
